4094715498206c2941aa444ce6d41a866d41887a
Sandra Vilder <sandra.vilder@gmail.com>
2020-11-19 18:52:06 +0100

    css and hard coded names

--- thefutureisfemale-front/src/components/App.js
+++ thefutureisfemale-front/src/components/App.js
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import thefutureisfemale_api from '../api/thefutureisfemale_api';
 import Sky from './Sky';
+import FemaleCharacters from './FemaleCharaters';
 import './App.css';
 
 const App = () => {
+  const [ female_characters, setFemale_characters ] = useState('');
   const search = async () => {
     const response = await thefutureisfemale_api.get('/api/v1/female_characters');
-    console.log(response.data);
-   }
-  search()
+    setFemale_characters(response.data);
+  }
+  //console.log(female_characters[0].name);
+  //search()
+  
   return (
     <div>
       <Sky />
-
+      <div className="container">
+        <FemaleCharacters />
+      </div>
       <svg height="100" width="100" className="moon">
         <circle cx="50" cy="50" r="50" fill="#F7F3E3" />
       </svg>
@@ -21,4 +27,3 @@ const App = () => {
 }
 
 export default App;
-
---
+++ thefutureisfemale-front/src/components/FemaleCharaters.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './FemaleCharacters.css';
+
+const FemaleCharacters = () => {
+  return (
+    <div className="card">
+      <p>Lyra</p>
+      <p>Arya</p>
+      <p>Hermione</p>
+    </div>
+  )
+}
+
+export default FemaleCharacters;
