eb7dc280faa258c1015c937fe719d6c3dae7aac9
Sandra Vilder <sandra.vilder@gmail.com>
2020-11-20 15:46:18 +0100

    tried something

--- thefutureisfemale-front/src/components/App.js
+++ thefutureisfemale-front/src/components/App.js
@@ -6,18 +6,19 @@ import './App.css';
 
 const App = () => {
   const [ female_characters, setFemale_characters ] = useState('');
+
   const search = async () => {
     const response = await thefutureisfemale_api.get('/api/v1/female_characters');
     setFemale_characters(response.data);
   }
   //console.log(female_characters[0].name);
   //search()
-  
+
   return (
     <div>
       <Sky />
       <div className="container">
-        <FemaleCharacters />
+        <FemaleCharacters female_characters={female_characters} />
       </div>
       <svg height="100" width="100" className="moon">
         <circle cx="50" cy="50" r="50" fill="#F7F3E3" />
