9bfcfe56bbd178bacc2685e6e3595cd56472a845
Sandra Vilder <sandra.vilder@gmail.com>
2020-11-27 17:50:10 +0100

    added links to check infos on books

--- thefutureisfemale-front/src/components/FemaleCharaters.js
+++ thefutureisfemale-front/src/components/FemaleCharaters.js
@@ -4,63 +4,62 @@ import './FemaleCharacters.css';
 const FemaleCharacters = () => {
   return (
     <div className="card">
-      <p><strong>Mariam et Laila</strong> - dans "Mille soleils splendides"</p>
-      <p><strong>Gerda</strong> - dans "La Reine des Neiges"</p>
-      <p><strong>Scarlett</strong> - dans "Autant en emporte le vent"</p>
-      <p><strong>Gervaise</strong> - dans "L'Assommoir"</p>
-      <p><strong>Sophie</strong> - dans "Les Malheurs de Sophie"</p>
-      <p><strong>Antigone</strong> - d'Anouilh</p>
-      <p><strong>Julia</strong> - dans "1984"</p>
-      <p><strong>Mafalda</strong></p>
-      <p><strong>Fantine</strong> - dans les "Les Misérables"</p>
-      <p><strong>Kay Scarpetta</strong> - de Patricia Cornwell</p>
-      <p><strong>Miss Courtney</strong> - dans "À la croisée des Mondes"</p>
-      <p><strong>Ciri et Yennefer</strong> - dans "Sorceleur"</p>
-      <p><strong>Arya et Cersei</strong> - dans "Le Trône de fer"</p>
-      <p><strong>Médée</strong> - d'Euripide</p>
-      <p><strong>Nynaeve</strong> - dans "La Roue du Temps"</p>
-      <p><strong>Esmé Ciredutemps</strong> - de Pratchett</p>
-      <p><strong>Polgara</strong> - dans la "Belgariade"</p>
-      <p><strong>Mélusine</strong></p>
-      <p><strong>Nelly Bly</strong> - dans "Dix jours dans un asile"</p>
-      <p><strong>Clémentine Delait</strong> - dans "Madame Delait, la femme à barbre"</p>
-      <p><strong>Lyra Belacqua</strong> - dans "À la croisée des Mondes"</p>
-      <p><strong>Elli H. Radinger</strong> - dans sa biographie "la sagesse des loups"</p>
-      <p><strong>Fifi Brindacier</strong></p>
-      <p><strong>Anna Karenine</strong></p>
-      <p><strong>Yoko Tsuno</strong></p>
-      <p><strong>Sorcière camomille</strong></p>
-      <p><strong>Emma Bovary</strong> - dans "Madame Bovary"</p>
-      <p><strong>Jeanne</strong> - dans "Une vie" de Maupassant</p>
-      <p><strong>Hermione</strong> - dans "Harry Potter"</p>
-      <p><strong>Alice</strong> - dans "Alice au pays des merveilles"</p>
-      <p><strong>Miss Marple</strong> - dans les romans policiers d'Agatha Christie</p>
-      <p><strong>Miss Lemon</strong> - dans "Hercule Poirot"</p>
-      <p><strong>Iseut</strong> - dans "Tristant et Iseut"</p>
-      <p><strong>Sophia Kovalevskaya</strong>- dans son autobiographie</p>
-      <p><strong>Antigone</strong></p>
-      <p><strong>Sudie</strong> - de Sara Flanigan</p>
-      <p><strong>May Dodd</strong> - dans "Mille femmes blanches"</p>
-      <p><strong>Maya Angelou</strong> - dans son roman autobiographique "Un billet d'avion pour l'Afrique"</p>
-      <p><strong>Tracy</strong> - dans "Sauvage" de Jamey Bradbury</p>
-      <p><strong>Les héroïnes de Jhumpa Lahiri</strong></p>
-      <p><strong>Les personnages de Jane Austen</strong></p>
-      <p><strong>Les personnages</strong> - dans "Les petites déesses #9" de Francesca Lia Block</p>
-      <p><strong>Anne Frank</strong> - dans son "Journal"</p>
-      <p><strong>Àntonia</strong> - dans "Mon Àntonia" de Willa Cather</p>
-      <p><strong>Ayla</strong> - dans "Les enfants de la Terre"</p>
-      <p><strong>La psy</strong> - dans "L'enfant bleu" de Henry Bauchau</p>
-      <p><strong>Sheeter</strong> - dans "la couleur des sentiments"</p>
-      <p><strong>La mère</strong> - dans "Jamais sans ma fille"</p>
-      <p><strong>L'héroïne</strong> - dans "la femme feuille"</p>
-      <p><strong>Simone Veil</strong> - dans "Une vie"</p>
-      <p><strong>Malala</strong> - dans "Moi, Malala"</p>
-      <p><strong>Smita</strong> - dans "La tresse"</p>
-      <p><strong>La mère de Gabriel</strong> - dans "Petit Pays"</p>
-      <p><strong>Ortie</strong> - dans "la Citadelle des ombres"</p>
-      <p><strong>Althéa</strong> - dans "les Aventuriers de la mer"</p>
-      <p><strong>L'enfant</strong> - dans "L'Amant"</p>
-      <p><strong>Constance</strong> - dans "Lady Chatterley's lover"</p>
+      <p><strong>Mariam et Laila</strong> - dans <a target="_blank" href="https://www.babelio.com/livres/Hosseini-Mille-soleils-splendides/32013">"Mille soleils splendides"</a></p>
+      <p><strong>Gerda</strong> - dans <a target="_blank" href="https://www.babelio.com/livres/Andersen-La-reine-des-neiges/106694">"La Reine des Neiges"</a></p>
+      <p><strong>Scarlett</strong> - dans <a target="_blank" href="https://www.babelio.com/livres/Mitchell-Autant-en-emporte-le-vent/25813">"Autant en emporte le vent"</a></p>
+      <p><strong>Gervaise</strong> - dans <a target="_blank" href="https://www.babelio.com/livres/Autiquet-LAssommoir/650021">"L'Assommoir"</a></p>
+      <p><strong>Sophie</strong> - dans <a target="_blank" href="https://www.babelio.com/livres/Segur-Les-malheurs-de-Sophie/15997">"Les Malheurs de Sophie"</a></p>
+      <p><a target="_blank" href="https://www.babelio.com/livres/Anouilh-Antigone/1724"><strong>Antigone</strong></a> - d'Anouilh</p>
+      <p><strong>Julia</strong> - dans <a target="_blank" href="https://www.babelio.com/livres/Orwell-1984/2961">"1984"</a></p>
+      <p><a target="_blank" href="https://fr.wikipedia.org/wiki/Mafalda"><strong>Mafalda</strong></a></p>
+      <p><strong>Fantine</strong> - dans <a target="_blank" href="https://www.babelio.com/livres/Hugo-Les-Miserables/1236116">"Les Misérables"</a></p>
+      <p><a target="_blank" href="https://www.babelio.com/livres/Cornwell-Kay-Scarpetta-Integrale-tome-1/134488"><strong>Kay Scarpetta</strong></a> - de Patricia Cornwell</p>
+      <p><strong>Miss Courtney</strong> - dans <a target="_blank" href="https://www.babelio.com/livres/Pullman--la-Croisee-des-Mondes--Integrale/12803">"À la croisée des Mondes"</a></p>
+      <p><strong>Ciri et Yennefer</strong> - dans <a target="_blank" href="https://www.babelio.com/livres/Sapkowski-Le-Sorceleur-tome-1--Le-Dernier-Voeu-reedition/259198">"Sorceleur"</a></p>
+      <p><strong>Arya et Cersei</strong> - dans <a target="_blank" href="https://www.babelio.com/livres/Martin-Le-Trone-de-Fer-Integrale-1--A-Game-of-Thrones/158547">"Le Trône de fer"</a></p>
+      <p><a target="_blank" href="https://www.babelio.com/livres/Euripide-Medee/20612"><strong>Médée</strong></a> - d'Euripide</p>
+      <p><strong>Nynaeve</strong> - dans <a target="_blank" href="https://www.babelio.com/livres/Jordan-La-Roue-du-Temps-Integrale-1--LOeil-du-Monde/356138">"La Roue du Temps"</a></p>
+      <p><a target="_blank" href="https://www.babelio.com/livres/Pratchett-Les-Annales-du-Disque-Monde-Tome-6--Trois-Soeurci/7769"><strong>Esmé Ciredutemps</strong> - de Pratchett</a></p>
+      <p><strong>Polgara</strong> - dans <a target="_blank" href="https://www.babelio.com/livres/Eddings-La-Belgariade-tome-1--Le-pion-blanc-des-presages/44956">la "Belgariade"</a></p>
+      <p><a target="_blank" href="https://www.babelio.com/livres/Gilson-Melusine-Tome-1--Sortileges/27970"><strong>Mélusine</strong></a></p>
+      <p><strong>Nellie Bly</strong> - dans <a target="_blank" href="https://www.babelio.com/livres/Bly-10-jours-dans-un-asile/790508">"10 jours dans un asile"</a></p>
+      <p><strong>Clémentine Delait</strong> - dans <a target="_blank" href="https://www.babelio.com/livres/Pasky-Clementine-une-femme-au-poil/959502">"Madame Delait, la femme à barbre"</a></p>
+      <p><strong>Lyra Belacqua</strong> - dans <a target="_blank" href="https://www.babelio.com/livres/Pullman--la-Croisee-des-Mondes--Integrale/12803">"À la croisée des Mondes"</a></p>
+      <p><strong>Elli H. Radinger</strong> - dans sa biographie <a target="_blank" href="https://www.babelio.com/livres/Radinger-La-sagesse-des-loups--Comment-ils-pensent-sorgan/1036670">"la sagesse des loups"</a></p>
+      <p><a target="_blank" href="https://www.babelio.com/livres/Lindgren-Fifi-Brindacier--Lintegrale/124885"><strong>Fifi Brindacier</strong></a></p>
+      <p><a target="_blank" href="https://www.babelio.com/livres/Tolstoi-Anna-Karenine/875941"><strong>Anna Karenine</strong></a></p>
+      <p><a target="_blank" href="https://www.babelio.com/livres/Leloup-Yoko-Tsuno-tome-1--Le-trio-de-letrange/11397"><strong>Yoko Tsuno</strong></a></p>
+      <p><a target="_blank" href="https://www.babelio.com/livres/Larreula-La-Sorciere-Camomille-Les-oeuvres-completes/147942"><strong>Sorcière camomille</strong></a></p>
+      <p><strong>Emma Bovary</strong> - dans <a target="_blank" href="https://www.babelio.com/livres/Flaubert-Madame-Bovary/894329">"Madame Bovary"</a></p>
+      <p><strong>Jeanne</strong> - dans <a target="_blank" href="https://www.babelio.com/livres/Maupassant-Une-vie/20150">"Une vie" de Maupassant</a></p>
+      <p><strong>Hermione</strong> - dans <a target="_blank" href="https://www.babelio.com/livres/Rowling-Harry-Potter-tome-1--Harry-Potter-a-lEcole-des-S/18811">"Harry Potter"</a></p>
+      <p><strong>Alice</strong> - dans <a target="_blank" href="https://www.babelio.com/livres/Carroll-Alice-au-pays-des-merveilles/3085">"Alice au pays des merveilles"</a></p>
+      <p><strong>Miss Marple et Miss Lemon</strong> - dans <a target="_blank" href="https://www.babelio.com/livres/Christie-Mort-sur-le-Nil/4562">les romans policiers d'Agatha Christie</a></p>
+      <p><strong>Iseut</strong> - dans <a target="_blank" href="https://fr.wikipedia.org/wiki/Tristan_et_Iseut">"Tristant et Iseut"</a></p>
+      <p><strong>Sophia Kovalevskaya</strong>- dans <a target="_blank" href="https://www.babelio.com/livres/Kovalevskaia-Kovalevskaia--Laventure-dune-mathematicienne/129578">son autobiographie</a></p>
+      <p><a target="_blank" href="https://fr.wikipedia.org/wiki/Antigone"><strong>Antigone</strong></a></p>
+      <p><a target="_blank" href="https://www.babelio.com/livres/Flanigan-Sudie/102473"><strong>Sudie</strong> - de Sara Flanigan</a></p>
+      <p><strong>May Dodd</strong> - dans <a target="_blank" href="https://www.babelio.com/livres/Fergus-Mille-femmes-blanches/16951">"Mille femmes blanches"</a></p>
+      <p><strong>Maya Angelou</strong> - dans son roman autobiographique <a target="_blank" href="https://www.babelio.com/livres/Angelou-Un-billet-davion-pour-lAfrique/281343">"Un billet d'avion pour l'Afrique"</a></p>
+      <p><strong>Tracy</strong> - dans <a target="_blank" href="https://www.babelio.com/livres/Bradbury-Sauvage/1111399">"Sauvage"</a> de Jamey Bradbury</p>
+      <p><a target="_blank" href="https://www.babelio.com/livres/Lahiri-Linterprete-des-maladies/158889"><strong>Les héroïnes de Jhumpa Lahiri</strong></a></p>
+      <p><a target="_blank" href="https://www.babelio.com/livres/Austen-Orgueil-et-prejuges/3887"><strong>Les personnages de Jane Austen</strong></a></p>
+      <p><strong>Les personnages</strong> - dans <a target="_blank" href="https://www.babelio.com/livres/Block-Les-petites-deesses/278549">"Les petites déesses"</a> de Francesca Lia Block</p>
+      <p><strong>Anne Frank</strong> - dans <a target="_blank" href="https://www.babelio.com/livres/Frank-Journal-dAnne-Frank/706783">son "Journal"</a></p>
+      <p><strong>Àntonia</strong> - dans <a target="_blank" href="https://www.babelio.com/livres/Cather-Mon-Antonia/18142">"Mon Àntonia"</a> de Willa Cather</p>
+      <p><strong>Ayla</strong> - dans <a target="_blank" href="https://www.babelio.com/livres/Auel-Les-enfants-de-la-Terre-tome-1--Le-clan-de-lours/1183179">"Les enfants de la Terre"</a></p>
+      <p><strong>La psy</strong> - dans <a target="_blank" href="https://www.babelio.com/livres/Bauchau-Lenfant-bleu/32840">"L'enfant bleu"</a> de Henry Bauchau</p>
+      <p><strong>Sheeter</strong> - dans <a target="_blank" href="https://www.babelio.com/livres/Stockett-La-couleur-des-sentiments/191520">"la couleur des sentiments"</a></p>
+      <p><strong>La mère</strong> - dans <a target="_blank" href="https://www.babelio.com/livres/Mahmoody-Jamais-sans-ma-fille/16844">"Jamais sans ma fille"</a></p>
+      <p><strong>L'héroïne</strong> - dans <a target="_blank" href="https://www.babelio.com/livres/Herve-Gruyer-La-Femme-Feuille/193503">"la femme feuille"</a></p>
+      <p><strong>Simone Veil</strong> - dans <a target="_blank" href="https://www.babelio.com/livres/Veil-Une-vie/36438">"Une vie"</a></p>
+      <p><strong>Malala</strong> - dans <a target="_blank" href="https://www.babelio.com/livres/Yousafzai-Moi-Malala/531546">"Moi, Malala"</a></p>
+      <p><strong>Smita</strong> - dans <a target="_blank" href="https://www.babelio.com/livres/Colombani-La-tresse/946395">"La tresse"</a></p>
+      <p><strong>La mère de Gabriel</strong> - dans <a target="_blank" href="https://www.babelio.com/livres/Faye-Petit-pays/847696">"Petit Pays"</a></p>
+      <p><strong>Ortie</strong> - dans <a target="_blank" href="https://www.babelio.com/livres/Hobb-La-Citadelle-des-Ombres-tome-1/22383">"la Citadelle des ombres"</a></p>
+      <p><strong>Althéa</strong> - dans <a target="_blank" href="https://www.babelio.com/livres/Hobb-Les-Aventuriers-de-la-mer-Tome-1--Le-vaisseau-mag/104388">"les Aventuriers de la mer"</a></p>
+      <p><strong>L'enfant</strong> - dans <a target="_blank" href="https://www.babelio.com/livres/Duras-Lamant/5772">"L'Amant"</a></p>
+      <p><strong>Constance</strong> - dans <a target="_blank" href="https://www.babelio.com/livres/Lawrence-LAmant-de-Lady-Chatterley/1644">"Lady Chatterley's lover"</a></p>
       <p><strong></strong></p>
       <p><strong></strong></p>
       <p><strong></strong></p>
